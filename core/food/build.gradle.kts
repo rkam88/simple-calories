0c5df5e839e1b2ea88e6153217f1a2bd167ed551
Roman Kamyshnikov <rkam88@gmail.com>
2023-12-22 07:33:05 +0000

    SQLDelight: add dependencies

--- core/food/build.gradle.kts
+++ core/food/build.gradle.kts
@@ -1,15 +1,36 @@
 plugins {
     id("langfordapps.compose.library")
+    alias(libs.plugins.sqldelight)
 }
 
 kotlin {
+    sourceSets.androidMain {
+        dependencies {
+            implementation(libs.sqldelight.android.driver)
+        }
+    }
     sourceSets.commonMain {
         dependencies {
             implementation(project(":core:utils"))
+        }
+    }
+    sourceSets.iosMain {
+        dependencies {
+            implementation(libs.sqldelight.native.driver)
         }
     }
 }
 
 android {
     namespace = "com.langfordapps.simplecalories.core.food"
+}
+
+// generate Database files with ./gradlew generateSqlDelightInterface
+sqldelight {
+    databases {
+        create("SimpleCaloriesDatabase") {
+            packageName = "com.langfordapps.simplecalories"
+            schemaOutputDirectory = file("src/commonMain/sqldelight")
+        }
+    }
 }
